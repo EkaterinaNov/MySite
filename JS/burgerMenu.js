f5bec0c8a80d493f30bff795317442a337f493ec
EkaterinaNov <ekaterinagnv@gmail.com>
2020-09-17 14:24:10 +0300

    added anchor to move up, max-width to body and something else

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -1,10 +1,14 @@
 function burgerMenuButtonClick(selector, defaultClass)
 {
+    
     var nonActiveMenuIcon = ' icon-menu';
     var activeMenuIcon = ' icon-cancel';
-
+    var selectorTop = 'idTop';
+    
     var menu = document.querySelector('.' + selector);
     var menuButton = document.querySelector('.' + selector + ' ' + '.' + defaultClass);
+
+    var divIdTop = document.querySelector('.' + selectorTop);
 
     //menuButton.preventDefault();
 
@@ -14,6 +18,7 @@ function burgerMenuButtonClick(selector, defaultClass)
         menu.style.right = '0';
         menu.style.width = '70%';
         menu.style.display = 'flex';
+        divIdTop.style.display = 'none';
     }
 
     else
@@ -21,10 +26,11 @@ function burgerMenuButtonClick(selector, defaultClass)
         menuButton.className = defaultClass + nonActiveMenuIcon;
         menu.style.right = '-16em';
         menu.style.width = '16em';
+        divIdTop.style.display = 'flex';
     }
 }
 
-function burgerMenuDefaultView (selector, defaultClass)
+/*function burgerMenuDefaultView (selector, defaultClass)
 {
     var nonActiveMenuIcon = ' icon-menu';
 
@@ -34,4 +40,4 @@ function burgerMenuDefaultView (selector, defaultClass)
     menuButton.className = defaultClass + nonActiveMenuIcon;
     menu.style.right = '-16em';
     menu.style.width = '16em';
-}
+}*/
